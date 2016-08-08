b72ff259b262aca530581f3b025e08602e5771be
loveKino <842914439@qq.com>
2016-08-08 17:27:02 +0800

    init

---
+++ index.js
@@ -0,0 +1,87 @@
+'use strict';
+
+let {
+    isObject, funType, or, isString, isFalsy
+} = require('basetype');
+
+let iterate = require('./lib/iterate');
+
+let {
+    map, reduce, find, findIndex, forEach
+} = require('./lib/fp');
+
+let contain = (list, item, fopts) => findIndex(list, item, fopts) !== -1;
+
+let difference = (list1, list2, fopts) => {
+    return reduce(list1, (prev, item) => {
+        if (!contain(list2, item, fopts) &&
+            !contain(prev, item, fopts)) {
+            prev.push(item);
+        }
+        return prev;
+    }, []);
+};
+
+let union = (list1, list2, fopts) => deRepeat(list2, fopts, deRepeat(list1, fopts));
+
+let mergeMap = (map1 = {}, map2 = {}) => reduce(map2, setValueKey, reduce(map1, setValueKey, {}));
+
+let setValueKey = (obj, value, key) => {
+    obj[key] = value;
+    return obj;
+};
+
+let interset = (list1, list2, fopts) => {
+    return reduce(list1, (prev, cur) => {
+        if (contain(list2, cur, fopts)) {
+            prev.push(cur);
+        }
+        return prev;
+    }, []);
+};
+
+let deRepeat = (list, fopts, init = []) => {
+    return reduce(list, (prev, cur) => {
+        if (!contain(prev, cur, fopts)) {
+            prev.push(cur);
+        }
+        return prev;
+    }, init);
+};
+
+/**
+ * a.b.c
+ */
+let get = funType((sandbox, name = '') => {
+    name = name.trim();
+    let parts = !name ? [] : name.split('.');
+    return reduce(parts, getValue, sandbox, invertLogic);
+}, [
+    isObject,
+    or(isString, isFalsy)
+]);
+
+let getValue = (obj, key) => obj[key];
+
+let invertLogic = v => !v;
+
+let delay = (time) => new Promise((resolve) => {
+    setTimeout(resolve, time);
+});
+
+module.exports = {
+    contain,
+    difference,
+    union,
+    interset,
+    map,
+    reduce,
+    iterate,
+    find,
+    findIndex,
+    deRepeat,
+    forEach,
+    get,
+    delay,
+    mergeMap
+};
---
+++ test/index.js
@@ -0,0 +1,146 @@
+'use strict';
+
+let assert = require('assert');
+
+let {
+    map, findIndex, reduce, contain, difference, union, interset, forEach, get, mergeMap
+} = require('../index');
+
+describe('index', () => {
+    it('map', () => {
+        assert.deepEqual(map([1, 2, 3, 4], (item) => item + 1), [2, 3, 4, 5]);
+        assert.deepEqual(map({
+            a: 1,
+            b: 2
+        }, (item) => item + 1), [2, 3]);
+
+        assert.deepEqual(map(['a', 'b'], (item, index) => {
+            if (index === 0) return 0;
+            return item;
+        }), [0, 'b']);
+
+        assert.deepEqual(map([], v => v), []);
+    });
+
+    it('findIndex', () => {
+        assert.equal(findIndex([3, 6, 8], 3), 0);
+        assert.equal(findIndex([3, 6, 8], 6), 1);
+        assert.equal(findIndex([3, 6, 8], 7, {
+            eq: (v1, v2) => v1 + 1 === v2
+        }), 2);
+        assert.equal(findIndex([3, 6, 8], 7, {
+            eq: (v1, v2) => v1 - 1 === v2
+        }), 1);
+
+        assert.equal(findIndex([3, 6, 8], 11), -1);
+    });
+
+    it('reduce', () => {
+        assert.equal(reduce([1, 2, 3], (prev, cur) => {
+            return prev + cur;
+        }, 0), 6);
+
+        assert.equal(reduce([], () => {}, 30), 30);
+
+        // reduce with limit
+        assert.equal(reduce([1, 2, 3, 4, 5], (prev, cur) => prev + cur, 0, (prev, item) => item >= 4), 6);
+    });
+
+    it('contain', () => {
+        assert.equal(contain([2, 45, 0, 8], 8), true);
+        assert.equal(contain([2, 45, 0, 8], -1), false);
+        assert.equal(contain([{
+            value: 10
+        }, {
+            value: 6
+        }], 6, {
+            eq: (item, v) => {
+                return v.value === item;
+            }
+        }), true);
+
+        assert.equal(contain([{
+            value: 10
+        }, {
+            value: 6
+        }], 16, {
+            eq: (item, v) => {
+                return v.value === item;
+            }
+        }), false);
+
+    });
+
+    it('difference', () => {
+        assert.deepEqual(difference([2, 5, 9], [9, 10]), [2, 5]);
+        assert.deepEqual(difference([2, 5, 9], [9, 9, 10]), [2, 5]);
+    });
+
+    it('union', () => {
+        assert.deepEqual(union([2, 5, 9], [9, 10]), [2, 5, 9, 10]);
+        assert.deepEqual(union([2, 2, 5, 9], [9, 9, 10]), [2, 5, 9, 10]);
+    });
+
+    it('interset', () => {
+        assert.deepEqual(interset([2, 5, 9], [9, 10]), [9]);
+        assert.deepEqual(interset([2, 5, 9, 11], [9, 10, 11]), [9, 11]);
+        assert.deepEqual(interset([2, 5, 9, 11], [19, 100, 1]), []);
+    });
+
+    it('forEach', () => {
+        let sum = 0;
+        forEach([2, 3, 4], (cur) => {
+            sum += cur;
+        });
+        assert.equal(sum, 9);
+    });
+
+    it('forEach2', () => {
+        let sum = 0;
+        forEach([2, 3, 4], (cur) => {
+            sum += cur;
+            if (cur > 2) return true;
+        });
+        assert.equal(sum, 5);
+    });
+
+    it('get', () => {
+        assert.deepEqual(get({
+            a: 1
+        }, ''), {
+            a: 1
+        });
+
+        assert.deepEqual(get({
+            a: 1
+        }, 'a'), 1);
+
+        assert.deepEqual(get({
+            a: {
+                b: 2
+            }
+        }, 'a.b'), 2);
+
+        assert.deepEqual(get({
+            a: 1
+        }, 'd'), null);
+
+        assert.deepEqual(get({
+            a: 1
+        }, 'a.b.c'), null);
+    });
+
+    it('mergeMap', () => {
+        assert.deepEqual(mergeMap({
+            a: 1,
+            c: 3
+        }, {
+            a: 2,
+            b: 2
+        }), {
+            a: 2,
+            c: 3,
+            b: 2
+        });
+    });
+});
